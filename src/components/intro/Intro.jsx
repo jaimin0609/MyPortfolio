4239217d6ddeead8d3e3843404b208af38cc2dd7
jaimin0609 <42791730+jaimin0609@users.noreply.github.com>
2021-12-24 19:27:48 +1100

    commmit

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,12 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            <div className="left">
+               <div className="imagecontainer">
+                  <img src="assets/jp.png" alt="" /> 
+               </div>            
+            </div>
+            <div className="right"></div>
             
         </div>
     )
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,7 @@ import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className={"topbar" + (menuOpen && "active")}>
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">Jaimin</a>
@@ -18,7 +18,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
                 </div>
                 
                 <div className="right">
-                    <div className="hamburger">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
                         <span className="line1"></span>
                         <span className="line2"></span>
                         <span className="line3"></span>
