cf3f90bfb6fd5012fb54780fc3259259b30e1c8e
jaimin0609 <42791730+jaimin0609@users.noreply.github.com>
2021-12-26 12:13:22 +1100

    commit

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,19 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, { 
+            showCursor: true, 
+            backDelay:  1500,
+            backSpeed: 60,
+            strings: ['Developer', 'Designer','Content Creator' ],
+         });
+    }, []);
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -8,7 +21,16 @@ export default function Intro() {
                   <img src="assets/man.png" alt="" /> 
                </div>            
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="wrapper">
+                   <h2>Hi There, I'm</h2>
+                   <h1>Jaimin Prajapati</h1>
+                   <h3>Business Analyst <span ref={textRef}></span></h3> 
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
             
         </div>
     )
